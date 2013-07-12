81cb3aea6af157784d088bc5310916d84d2d2289
Rohan Roy <b4you0870@gmail.com>
2013-07-12 16:09:02 +0000

    grader.js

--- grader.js
+++ grader.js
@@ -30,7 +30,7 @@ var CHECKSFILE_DEFAULT = "checks.json";
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
-        console.log("%s does not exist in the disk. Exiting.", instr);
+        console.log("%s does not exist. Exiting.", instr);
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;

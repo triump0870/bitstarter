50c3da7e54f4577eba86df35af9664f692a78a87
Rohan Roy <b4you0870@gmail.com>
2013-07-16 10:57:24 +0000

    Newly Updated web.js

--- web.js
+++ web.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var fs=require('fs');
 var htmlfile="index.html";
-//var text=fs.readFileSync('index.html',"utf-8")
+
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  	var html=fs.readFileSync(htmlfile).tostring();
+  	var html=fs.readFileSync(htmlfile).toString();
 	response.send(html);
 });
 
